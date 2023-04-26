e62b7e31b2066be5a9e504b15b84eee37cf66990
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-04-26 21:00:11 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Ярослав Любиченко on 26.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
