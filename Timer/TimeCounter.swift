5b5e8fa3fad92d6ef3f31c4cfc431d4a437c23c7
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-04-29 21:21:54 +0300

    TimeCounter.

---
+++ Timer/TimeCounter.swift
@@ -0,0 +1,60 @@
+//
+//  TimeCounter.swift
+//  Timer
+//
+//  Created by Ярослав Любиченко on 26.4.2023.
+//
+
+import Foundation
+import Combine
+
+final class TimeCounter: ObservableObject {
+    var counter = 3
+    var buttonTitle = "Start"
+    let objectWillChange = ObservableObjectPublisher()
+    
+    private var timer: Timer?
+    
+    func startTimer() {
+        if counter > 0 {
+            timer = Timer.scheduledTimer(
+                timeInterval: 1,
+                target: self,
+                selector: #selector(updateCounter),
+                userInfo: nil,
+                repeats: true
+            )
+        } else {
+            updateCounter()
+        }
+        
+        buttonDidTapped()
+    }
+    
+    @objc private func updateCounter() {
+        if counter > 0 {
+            counter -= 1
+        } else {
+            killTimer()
+            buttonTitle = "Reset"
+        }
+        
+        objectWillChange.send()
+    }
+    
+    private func buttonDidTapped() {
+        if buttonTitle == "Reset" {
+            counter = 3
+            buttonTitle = "Start"
+        } else {
+            buttonTitle = "Wait..."
+        }
+        
+        objectWillChange.send()
+    }
+    
+    private func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
