76e5164bf9f852b9447b7a4776628aec62aff93e
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-04-29 21:18:22 +0300

    Add LogOut Button(LogOutButtonView).

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var timer = TimeCounter()
+    @EnvironmentObject private var userSettings: UserSettings
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hi, \(userSettings.name)")
+                .font(.largeTitle)
+                .padding(.top, 100)
+            Text(timer.counter.formatted())
+                .font(.largeTitle)
+                .padding(.top, 100)
+            ButtonView(timer: timer)
+                .padding(.top, 50)
+            Spacer()
+            
+            LogOutButtonView()
         }
         .padding()
     }
@@ -22,5 +32,26 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(UserSettings())
+    }
+}
+
+struct ButtonView: View {
+    @ObservedObject var timer: TimeCounter
+    
+    var body: some View {
+        Button(action: timer.startTimer) {
+            Text(timer.buttonTitle)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(.red)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.black, lineWidth: 4)
+        )
     }
 }
